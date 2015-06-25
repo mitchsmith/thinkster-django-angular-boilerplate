da744830b36e07af9e501940d78ccf570ffc7ec9
John Mitchell Smith <mitch@jmitchellsmith.com>
2015-06-25 15:24:37 -0500

    Set up angular modules; enabled html5routing instead of hash routing; integrated angular and django csrf protection.

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,28 @@
 angular
-  .module('thinkster', []);
+    .module('thinkster', [
+        'thinkster.config',
+        'thinkster.routes',
+        'thinkster.authentication'
+    ]);
+
+angular
+    .module('thinkster.config', []);
+
+angular
+    .module('thinkster.routes', ['ngRoute']);
+
+/** csrf protection in concert with django **/
+angular
+  .module('thinkster')
+  .run(run);
+
+run.$inject = ['$http'];
+
+/**
+* @name run
+* @desc Update xsrf $http headers to align with Django's defaults
+*/
+function run($http) {
+  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $http.defaults.xsrfCookieName = 'csrftoken';
+}
